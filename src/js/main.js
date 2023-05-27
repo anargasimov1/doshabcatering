9b24c9ccac2a580e15abc7df21439c800485cc2a
anargasimov1 <anar.gasimov.89@gmail.com>
2023-05-27 20:12:17 +0400

    v1.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const title2 = document.getElementById("title2"),
-    title1 = document.getElementById("title1");
+    title1 = document.getElementById("title1"),
+    btn_open = document.getElementById("btn"),
+    btn_close = document.querySelector(".btn-danger"),
+    navbar = document.getElementById("navbar");
 
 setInterval(() => {
     title1.style.opacity = 0;
     title2.style.opacity = 1;
- 
+
 }, 3000)
 
 
@@ -15,3 +18,9 @@ setInterval(() => {
     title2.style.opacity = 0;
 
 }, 6000)
+
+btn_open.addEventListener("click", () => navbar.style.width = "100%")
+
+btn_close.addEventListener("click",()=>{
+    navbar.style.width = "0"
+})
